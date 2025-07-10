c764b4064624254f4885059c8142f28efe0df2e3
Adrian Mieńkowski <amienkowski@gmail.com>
2025-07-10 12:21:16 +0200

    psql added to docker-compose

--- src/main/java/com/project/hms/HotelManagementSystemApplication.java
+++ src/main/java/com/project/hms/HotelManagementSystemApplication.java
@@ -44,29 +44,28 @@ public class HotelManagementSystemApplication implements CommandLineRunner {
 
 		reservationDao.save(
 				new ReservationEntity("Adrian", "M", LocalDate.now(), LocalDate.now().plusDays(7)));
-
 		hotelListDao.save(
 				new HotelEntity("Hotel Karpacz", "Karpacz", CountryEnum.POLAND, "30-015 Karpacz", 269.90));
 		hotelListDao.save(
-				new HotelEntity("Hotel Kłaj", "Kłaj", CountryEnum.POLAND, "32-015 Kłaj", 145.00));
+				new HotelEntity("Hotel Kraków", "Kraków", CountryEnum.POLAND, "32-010 Kraków", 145.00));
 		hotelListDao.save(
-				new HotelEntity("Hotel Szarów", "Szarów", CountryEnum.POLAND, "32-015 Kłaj", 169.90));
+				new HotelEntity("Hotel Nowy Sącz", "Nowy Sącz", CountryEnum.POLAND, "30-028 Nowy Sącz", 169.90));
 		hotelListDao.save(
-				new HotelEntity("Burj Al Arab", "Dubaj", CountryEnum.UAE, "32-015 Kłaj", 450.00));
+				new HotelEntity("Burj Al Arab", "Dubaj", CountryEnum.UAE, "22-001 Warszawa", 450.00));
 		hotelListDao.save(
-				new HotelEntity("The Ritz-Carlton", "Nowy Jork", CountryEnum.USA, "32-015 Kłaj", 178.99));
+				new HotelEntity("The Ritz-Carlton", "Nowy Jork", CountryEnum.USA, "22-001 Warszawa", 178.99));
 		hotelListDao.save(
-				new HotelEntity("The Savoy", "Londyn", CountryEnum.GREAT_BRITAIN, "32-015 Kłaj", 245.00));
+				new HotelEntity("The Savoy", "Londyn", CountryEnum.GREAT_BRITAIN, "22-001 Warszawa", 245.00));
 		hotelListDao.save(
-				new HotelEntity("Aman Tokyo", "Tokio", CountryEnum.JAPAN, "32-015 Kłaj", 799.00));
+				new HotelEntity("Aman Tokyo", "Tokio", CountryEnum.JAPAN, "22-001 Warszawa", 799.00));
 		hotelListDao.save(
-				new HotelEntity("Hotel de Crillon", "Paryż", CountryEnum.FRANCE, "32-015 Kłaj", 650.99));
+				new HotelEntity("Hotel de Crillon", "Paryż", CountryEnum.FRANCE, "22-001 Warszawa", 650.99));
 		hotelListDao.save(
-				new HotelEntity("Shangri-La Hotel", "Singapur", CountryEnum.SINGAPORE, "32-015 Kłaj", 489.90));
+				new HotelEntity("Shangri-La Hotel", "Singapur", CountryEnum.SINGAPORE, "22-001 Warszawa", 489.90));
 		hotelListDao.save(
-				new HotelEntity("The Oberoi Udaivilas", "Udapur", CountryEnum.INDIA, "32-015 Kłaj", 500.00));
+				new HotelEntity("The Oberoi Udaivilas", "Udapur", CountryEnum.INDIA, "22-001 Warszawa", 500.00));
 		hotelListDao.save(
-				new HotelEntity("Four Seasons Hotel", "Toronto", CountryEnum.CANADA, "32-015 Kłaj", 248.00));
+				new HotelEntity("Four Seasons Hotel", "Toronto", CountryEnum.CANADA, "22-001 Warszawa", 248.00));
 
 	}
 }
